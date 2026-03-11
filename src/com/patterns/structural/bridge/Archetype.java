6c0c4044f43b0eaedecde943c80b8480ce3ccb0f
shyshkovskaliliia <shyshkovska.liliia@chnu.edu.ua>
2026-03-11 19:59:13 +0200

    create public interface archetype patternbridge

---
+++ src/com/patterns/structural/bridge/Archetype.java
@@ -0,0 +1,10 @@
+package com.patterns.structural.bridge;
+
+public class Archetype {
+    String getName();
+    String getPrimarySkill();
+    String attack();
+    String defend();
+    String specialAbility();
+    String getStats();
+}
