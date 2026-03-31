451dd668c023ceb52ac16b772cf5bfb033dfcdfc
shyshkovskaliliia <shyshkovska.liliia@chnu.edu.ua>
2026-03-31 22:09:06 +0300

    task 1 realisation

--- src/Lab4/Lambda_tasks/LambdaTasks.java
+++ src/Lab4/Lambda_tasks/LambdaTasks.java
@@ -1,9 +1,17 @@
 package Lab4.Lambda_tasks;
 
 import java.util.*;
-import java.util.stream.*;
-import java.util.function.*;
+import java.util.stream.Collectors*;
+import java.util.stream.IntStream*;
 
 public class LambdaTasks {
+
+    //task 1
+    public static List<Integer> task1(List<Integer> num){
+        return num.stream()
+                .filter(n -> n%2 !=0)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
 
 }
