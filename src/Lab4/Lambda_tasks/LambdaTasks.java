26c92df3f4ae3c751af947b32fc484df1b480e1c
shyshkovskaliliia <shyshkovska.liliia@chnu.edu.ua>
2026-03-31 22:11:52 +0300

    task 2  realisation

--- src/Lab4/Lambda_tasks/LambdaTasks.java
+++ src/Lab4/Lambda_tasks/LambdaTasks.java
@@ -13,5 +13,14 @@ public class LambdaTasks {
                 .collect(Collectors.toUnmodifiableList());
     }
 
+    //task 2
+    public static double task2(List<Double> num){
+        return num.stream()
+                .mapToDouble(n-> n)
+                .average()
+                .orElse(0.0);
+
+    }
+
 
 }
