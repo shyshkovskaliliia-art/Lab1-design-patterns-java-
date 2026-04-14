c2757f56b437926d282acdc29f80bd206f76d5cb
shyshkovskaliliia <shyshkovska.liliia@chnu.edu.ua>
2026-04-14 23:56:55 +0300

    execute around

---
+++ src/Lab5/design_patterns_revisited/revisited/execute_around/Resource.java
@@ -0,0 +1,35 @@
+package Lab5.design_patterns_revisited.revisited.execute_around;
+
+import java.util.function.Consumer;
+
+public class Resource {
+    rivate boolean open = false;
+    private Resource() {}
+
+    private void open() {
+        open = true;
+        System.out.println("Ресурс відкрито");
+    }
+
+    private void close() {
+        open = false;
+        System.out.println("Ресурс закрито");
+    }
+
+    public void doWork(String data) {
+        if (!open) {
+            throw new IllegalStateException("Ресурс не відкрито!");
+        }
+        System.out.println("⚙Обробка: " + data);
+    }
+
+    public static void use(Consumer<Resource> block) {
+        Resource resource = new Resource();
+        resource.open();
+        try {
+            block.accept(resource);
+        } finally {
+            resource.close();
+        }
+    }
+}
